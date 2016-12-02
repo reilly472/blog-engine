65e5c5fca83aa289ab9255c9657b6cd7594abcd3
Jimmy Reilly <reilly@whitewavemedia.com>
2016-12-02 23:01:16 +0000

    added simple forms

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
     validates :title, presence: true, length: {minimum: 5}
     validates :body, presence: true
-  
+    
+    def body_display
+      read_attribute(:body).html_safe
+    end
 end
