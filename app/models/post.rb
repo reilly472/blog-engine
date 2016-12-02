ac21c926987a230a0828c1eb12d5b8bb0dd04035
Jimmy Reilly <reilly@whitewavemedia.com>
2016-12-02 22:46:19 +0000

    removed display

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,5 @@
 class Post < ActiveRecord::Base
     validates :title, presence: true, length: {minimum: 5}
     validates :body, presence: true
-    
-    def body_display
-      Liquid::Template.parse(read_attribute(:body)).render.html_safe
-    end
+  
 end
