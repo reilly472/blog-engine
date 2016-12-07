9f8f99fd7515fc72fdbfb06485dcf05bc041c688
Jimmy Reilly <reilly@whitewavemedia.com>
2016-12-07 18:32:50 +0000

    added validations to snippets

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -2,4 +2,5 @@ class Snippet < ActiveRecord::Base
     validates :name, presence: true
     validates :body, presence: true
     validates :code, presence: true
+    validates_format_of :code, with: /\A[_a-z0-9]*\z/, message: "Code can only use lowercase letters, numbers, and underscores."
 end
